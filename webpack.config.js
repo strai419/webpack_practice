98c3c8d39b9a656f52cf0cf4c46436047e5e8184
strai419 <67533334+strai419@users.noreply.github.com>
2020-07-11 00:35:04 +0900

    Install Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,15 @@ module.exports = {
     module:{
         rules: [
             {
+                test: /\.(ts|tsx)/,
+                exclude: /node-modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+            {
                 test: /\.vue/,
                 exclude: /node-modules/,
                 use: [
